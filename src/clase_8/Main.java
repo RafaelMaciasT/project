73a90339c9c0c211cb7cbcb8ee149a377efcdd5e
RafaelMaciasT <maciasjesus03@gmail.com>
2024-09-09 10:11:12 -0300

    Saludo

--- src/clase_8/Main.java
+++ src/clase_8/Main.java
@@ -1,10 +1,12 @@
 package clase_8;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		JOptionPane.showMessageDialog(null, "Hola mundo");
 	}
 
 }
